c53205d5a79a0bce25f28a7fcc22b1c6d1ea1371
Henok <henokmicrometre@gmail.com>
2023-09-23 14:24:15 +0100

    adding landing

--- index.ts
+++ index.ts
@@ -1,9 +1,20 @@
+
+const path = "./package.json";
+const file = Bun.file(path);
+
+await file.exists(); // boolean;
+console.log(await file.exists())
+
 const ret: any[] = [];
 Bun.serve({
   async fetch(req: Request) {
     const url = new URL(req.url);
     if (url.pathname === "/") {
-      return new Response(ret);
+            return new Response(Bun.file("./public/index.html"), {
+        headers: {
+          "Content-Type": "text/html",
+        },
+      });
     }
     if (url.pathname === "/video") {
       return new Response(Bun.file("./alprVideo.mp4"));
@@ -13,6 +24,9 @@ Bun.serve({
       const data = reqBody.results[0].plate;
       ret.push(data);
       console.log(ret);
+      return new Response(ret);
+    }
+    if (url.pathname === "/plates"){
       return new Response(ret);
     }
     return new Response("404!");
